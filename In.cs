11670587b0ec21dc87c391684b87b3c750818b96
Cameron Thompson <39208507+cdthomp1@users.noreply.github.com>
2019-06-07 15:33:43 -0600

    New and improved

--- In.cs
+++ In.cs
@@ -1,38 +1,55 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using Newtonsoft.Json.Linq;
 
 namespace Wololo
 {
     class In
     {
+
         // Convert CSV file to JArray
         internal static JArray jarrFromCSVFile(string path)
         {
             JArray jarr = new JArray();
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader))
+            {
+                var records = csv.GetRecords<dynamic>();
+
+                foreach (dynamic rec in records)
+                {
+                    JObject job = JObject.FromObject(rec);
+
+                    jarr.Add(job);
+                }
+
+            }
             return jarr;
         }
 
         // Convert HTTP url request to JArray
-        internal static JArray jarrFromURL(string url)
-        {
-            JArray jarr = new JArray();
-            return jarr;
-        }
-
 
         // Convert JSON file to JArray 
         internal static JArray jarrFromJSONFile(string path)
         {
-            JArray jarr = new JArray();
+            string json = System.IO.File.ReadAllText(path);
+            JArray jarr = JArray.Parse(json);
             return jarr;
         }
 
         // Convert C# OBJ to JArray 
-        internal static JArray jarrFromObj(Object obj)
+        internal static JArray jarrFromObj(List<dynamic> objects)
         {
+            // loop through the list of objects and add them to the array.
             JArray jarr = new JArray();
-            return jarr.FromObject(obj);
+
+            foreach (dynamic obj in objects)
+            {
+                JObject job = JObject.FromObject(obj);
+                jarr.Add(job);
+            }
+            return jarr;
         }
-
-
     }
 }

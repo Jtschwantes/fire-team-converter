32944a9e20e88239cf6c7d3af3e2716289a21438
Cameron Thompson <39208507+cdthomp1@users.noreply.github.com>
2019-06-07 13:07:04 -0600

    Added IN

---
+++ In.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Wololo
+{
+    class In
+    {
+        // Convert CSV file to JArray
+        internal static JArray jarrFromCSVFile(string path)
+        {
+            JArray jarr = new JArray();
+            return jarr;
+        }
+
+        // Convert HTTP url request to JArray
+        internal static JArray jarrFromURL(string url)
+        {
+            JArray jarr = new JArray();
+            return jarr;
+        }
+
+
+        // Convert JSON file to JArray 
+        internal static JArray jarrFromJSONFile(string path)
+        {
+            JArray jarr = new JArray();
+            return jarr;
+        }
+
+        // Convert C# OBJ to JArray 
+        internal static JArray jarrFromObj(Object obj)
+        {
+            JArray jarr = new JArray();
+            return jarr.FromObject(obj);
+        }
+
+
+    }
+}
